f96a97f780403b2278157e639b64429647d0b8f9
tarvasa <60059249+tarvasa@users.noreply.github.com>
2020-10-13 15:56:43 +0300

    delete button hidden from other users than those who has role admin

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -71,6 +72,8 @@ public class BookController {
 		repository.save(book);
 		return "redirect:booklist";
 	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
