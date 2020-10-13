98231e43e4b999bdb2b940d3496304188fd43090
tarvasa <60059249+tarvasa@users.noreply.github.com>
2020-10-13 15:18:42 +0300

    WebSecurityConfig-file added to the project, two in-memory users created

---
+++ src/main/java/hh/swd20/bookstore/WebSecuirytConfig.java
@@ -0,0 +1,56 @@
+package hh.swd20.bookstore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecuirytConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+        .authorizeRequests().anyRequest().authenticated()
+        .and()
+      .formLogin()
+          .defaultSuccessUrl("/booklist")
+          .permitAll()
+          .and()
+      .logout()
+          .permitAll();
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		List<UserDetails> users = new ArrayList();
+		
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("user")
+				.roles("USER")
+				.build();
+		users.add(user);
+		
+		UserDetails admin = User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("admin")
+				.roles("USER", "ADMIN")
+				.build();
+		users.add(admin);
+		
+		return new InMemoryUserDetailsManager(users);
+	}
+
+
+}
