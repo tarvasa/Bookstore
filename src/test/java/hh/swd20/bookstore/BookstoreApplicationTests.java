e0c33e9fbf0b248bb581b7b30f53d2132cf09c37
tarvasa <60059249+tarvasa@users.noreply.github.com>
2020-10-21 09:53:21 +0300

    smoke tests with controllers added

--- src/test/java/hh/swd20/bookstore/BookstoreApplicationTests.java
+++ src/test/java/hh/swd20/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,35 @@
 package hh.swd20.bookstore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import hh.swd20.bookstore.web.BookController;
+import hh.swd20.bookstore.web.CategoryController;
+
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookstoreApplicationTests {
+	
+	@Autowired
+	private BookController bookController;
+	
+	@Autowired
+	private CategoryController categoryController;
 
 	@Test
-	void contextLoads() {
+	void bookContextLoads() throws Exception {
+		assertThat(bookController).isNotNull();
+	}
+	
+	@Test
+	void categoryContextLoads() throws Exception {
+		assertThat(categoryController).isNotNull();
 	}
 
 }
